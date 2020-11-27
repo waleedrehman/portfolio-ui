3b3fbd311d9596a9e40a6bbfe05e7ef0da0392ba
Waleed Ur Rehman <mac@waleeds-mbp.home>
2020-11-27 20:32:15 +0000

    Trying to load the static js files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import './css/bootstrap.css';
 import './css/ionicons.css';
@@ -8,6 +7,18 @@ import './css/responsive.css';
 import Header from './components/Header';
 import IntroSection from './components/IntroSection';
 import AboutSection from './components/AboutSection';
+import './common-js/jquery-3.2.1.min.js';
+import './common-js/tether.min.js';
+import './common-js/bootstrap.js';
+import './common-js/isotope.pkgd.min.js';
+import './common-js/jquery.waypoints.min.js';
+import './common-js/progressbar.min.js';
+import './common-js/jquery.fluidbox.min.js';
+import './common-js/scripts.js';
+import './common-js/init.js';
+
+
+
 
 function App() {
   return (
